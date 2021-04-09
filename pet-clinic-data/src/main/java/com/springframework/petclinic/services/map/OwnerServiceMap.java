960d8cbe276e4b4c02ccda9961353928ee490c40
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-04-09 14:15:48 +0800

    adding bootstrap for data loader. closes #14

--- pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package com.springframework.petclinic.services.map;
 
 import com.springframework.petclinic.model.Owner;
-import com.springframework.petclinic.services.CrudService;
+import com.springframework.petclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements CrudService<Owner,Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements OwnerService{
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -32,4 +32,8 @@ public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements C
 
     }
 
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
+    }
 }
--- pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"","/","/index","/index.html"})
+    @RequestMapping({"", "/","/index","/index.html"})
     public String listOwners(){
         return "owners/index";
     }

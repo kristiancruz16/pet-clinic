6190911e771575fb889b9f4986d83ace3de9502a
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-04-09 14:21:57 +0800

    adding spring configuration. closes #22

--- pet-clinic-web/src/main/java/com/springframework/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/PetClinicApplication.java
@@ -3,6 +3,7 @@ package com.springframework.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PetClinicApplication {
 

8c362c87d96c812c7b53185c2c86b7955ab515ad
kristiancruz16 <kristian_m_cruz12@yahoo.com>
2021-04-16 08:45:24 +0800

    update

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Pet.java
@@ -11,10 +11,22 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pet")
 public class Pet extends  BaseEntity{
+
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visit) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+
+        if (visit == null || visit.size() > 0 ) {
+            this.visit = visit;
+        }
+    }
 
     @Column(name = "name")
     private String name;
